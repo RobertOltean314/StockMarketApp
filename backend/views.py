245b75c866ade27e605cc0a38215b67a169a5e1c
Robert Oltean <olteanrobertadrian314@gmail.com>
2024-05-21 21:02:32 +0300

    Worked on Watchlist functionality

--- backend/views.py
+++ backend/views.py
@@ -1,7 +1,7 @@
 from django import forms
-from django.http import Http404, JsonResponse
-from django.shortcuts import redirect
-from django.urls import reverse_lazy
+from django.http import JsonResponse, HttpResponseRedirect
+from django.shortcuts import redirect, get_object_or_404, render
+from django.urls import reverse_lazy, reverse
 from django.views.generic import TemplateView
 from django.views.generic.edit import FormView, UpdateView
 from backend.forms import WatchlistForm
@@ -10,6 +10,7 @@ from backend.models import Stock, Watchlist
 
 class StockNameForm(forms.Form):
     stock_name = forms.CharField(max_length=20)
+
 
 class DashboardView(TemplateView):
     template_name = 'stocks/dashboard.html'
@@ -21,8 +22,8 @@ class DashboardView(TemplateView):
             return JsonResponse(data, safe=False)
         else:
             return super().get(request, *args, **kwargs)
-        
-        
+
+
 class StockGraphView(FormView):
     template_name = 'stocks/dashboard.html'
     form_class = StockNameForm
@@ -31,16 +32,28 @@ class StockGraphView(FormView):
         stock_name = request.GET.get('stock_name')
 
         return redirect('dashboard', stock_name=stock_name)
-    
+
 
 class HomePageView(TemplateView):
-    template_name='home/home.html'
-    
+    template_name = 'home/home.html'
+
+
 class WatchlistView(UpdateView):
-    model = Watchlist 
+    model = Watchlist
     form_class = WatchlistForm
     template_name = 'stocks/watchlist.html'
     success_url = reverse_lazy('watchlist')  # replace with your success url
 
     def get_object(self, queryset=None):
         return Watchlist.objects.get_or_create(user=self.request.user)[0]
+
+    def post(self, request, *args, **kwargs):
+        action = request.POST.get('action')
+        stock_name = request.POST.get('stock_name')
+        stock = Stock.objects.get(stock_name=stock_name)
+        watchlist = self.get_object()
+
+        if action == 'add':
+            watchlist.stocks.add(stock)
+
+        return HttpResponseRedirect(reverse('watchlist'))

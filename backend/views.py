f753edc07308272d97dada60d3ec9d1baaca5613
Robert Oltean <olteanrobertadrian314@gmail.com>
2024-05-21 23:41:04 +0300

    Final commit for now

--- backend/views.py
+++ backend/views.py
@@ -47,13 +47,22 @@ class WatchlistView(UpdateView):
     def get_object(self, queryset=None):
         return Watchlist.objects.get_or_create(user=self.request.user)[0]
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        watchlist = self.get_object()
+        context['stocks'] = [(stock, stock.get_most_recent_data()) for stock in watchlist.stocks.all()]
+        return context
+
     def post(self, request, *args, **kwargs):
         action = request.POST.get('action')
-        stock_name = request.POST.get('stock_name')
-        stock = Stock.objects.get(stock_name=stock_name)
+        stock_name = request.POST.get('stock_name').strip()  # Remove leading and trailing whitespaces
+        print(f"stock_name: {stock_name}")  # Print the stock_name to the console
+        stock = get_object_or_404(Stock, stock_name__iexact=stock_name)
         watchlist = self.get_object()
 
         if action == 'add':
             watchlist.stocks.add(stock)
+        elif action == 'remove':
+            watchlist.stocks.remove(stock)
 
         return HttpResponseRedirect(reverse('watchlist'))
